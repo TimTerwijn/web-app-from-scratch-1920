4dd1b27af082255f9d15d9acf1a9f7407981afd0
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-02-04 13:33:17 +0100

    fixed monster get and set bug

--- js/models/Monster.js
+++ js/models/Monster.js
@@ -35,36 +35,36 @@ export class Monster{
         this._defenceRanged = defenceRanged;
     }
 
-    set_name(_name) { this._name = _name; }
-    get_name() { return this._name; } 
-    set_attackLevel(_attackLevel,) { this._attackLevel = _attackLevel; } 
-    get_attackLevel() { return this._attackLevel; } 
-    set_strengthLevel(_strengthLevel,) { this._strengthLevel = _strengthLevel; } 
-    get_strengthLevel() { return this._strengthLevel; } 
-    set_defenceLevel(_defenceLevel,) { this._defenceLevel = _defenceLevel; } 
-    get_defenceLevel() { return this._defenceLevel; } 
-    set_magicLevel(_magicLevel,) { this._magicLevel = _magicLevel; } 
-    get_magicLevel() { return this._magicLevel; } 
-    set_rangedLevel(_rangedLevel,) { this._rangedLevel = _rangedLevel; } 
-    get_rangedLevel() { return this._rangedLevel; } 
-    set_attackStab(_attackStab,) { this._attackStab = _attackStab; } 
-    get_attackStab() { return this._attackStab; } 
-    set_attackSlash(_attackSlash,) { this._attackSlash = _attackSlash; } 
-    get_attackSlash() { return this._attackSlash; } 
-    set_attackCrush(_attackCrush,) { this._attackCrush = _attackCrush; } 
-    get_attackCrush() { return this._attackCrush; } 
-    set_attackMagic(_attackMagic,) { this._attackMagic = _attackMagic; } 
-    get_attackMagic() { return this._attackMagic; } 
-    set_attackRanged(_attackRanged,) { this._attackRanged = _attackRanged; } 
-    get_attackRanged() { return this._attackRanged; } 
-    set_defenceStab(_defenceStab,) { this._defenceStab = _defenceStab; } 
-    get_defenceStab() { return this._defenceStab; } 
-    set_defenceSlash(_defenceSlash,) { this._defenceSlash = _defenceSlash; } 
-    get_defenceSlash() { return this._defenceSlash; } 
-    set_defenceCrush(_defenceCrush,) { this._defenceCrush = _defenceCrush; } 
-    get_defenceCrush() { return this._defenceCrush; } 
-    set_defenceMagic(_defenceMagic,) { this._defenceMagic = _defenceMagic; } 
-    get_defenceMagic() { return this._defenceMagic; } 
-    set_defenceRanged(_defenceRanged) { this._defenceRanged = _defenceRanged}
-    get_defenceRanged() { return this._defenceRanged}
+    setName(_name) { this._name = _name; }
+    getName() { return this._name; } 
+    setAttackLevel(_attackLevel,) { this._attackLevel = _attackLevel; } 
+    getAttackLevel() { return this._attackLevel; } 
+    setStrengthLevel(_strengthLevel,) { this._strengthLevel = _strengthLevel; } 
+    getStrengthLevel() { return this._strengthLevel; } 
+    setDefenceLevel(_defenceLevel,) { this._defenceLevel = _defenceLevel; } 
+    getDefenceLevel() { return this._defenceLevel; } 
+    setMagicLevel(_magicLevel,) { this._magicLevel = _magicLevel; } 
+    getMagicLevel() { return this._magicLevel; } 
+    setRangedLevel(_rangedLevel,) { this._rangedLevel = _rangedLevel; } 
+    getRangedLevel() { return this._rangedLevel; } 
+    setAttackStab(_attackStab,) { this._attackStab = _attackStab; } 
+    getAttackStab() { return this._attackStab; } 
+    setAttackSlash(_attackSlash,) { this._attackSlash = _attackSlash; } 
+    getAttackSlash() { return this._attackSlash; } 
+    setAttackCrush(_attackCrush,) { this._attackCrush = _attackCrush; } 
+    getAttackCrush() { return this._attackCrush; } 
+    setAttackMagic(_attackMagic,) { this._attackMagic = _attackMagic; } 
+    getAttackMagic() { return this._attackMagic; } 
+    setAttackRanged(_attackRanged,) { this._attackRanged = _attackRanged; } 
+    getAttackRanged() { return this._attackRanged; } 
+    setDefenceStab(_defenceStab,) { this._defenceStab = _defenceStab; } 
+    getDefenceStab() { return this._defenceStab; } 
+    setDefenceSlash(_defenceSlash,) { this._defenceSlash = _defenceSlash; } 
+    getDefenceSlash() { return this._defenceSlash; } 
+    setDefenceCrush(_defenceCrush,) { this._defenceCrush = _defenceCrush; } 
+    getDefenceCrush() { return this._defenceCrush; } 
+    setDefenceMagic(_defenceMagic,) { this._defenceMagic = _defenceMagic; } 
+    getDefenceMagic() { return this._defenceMagic; } 
+    setDefenceRanged(_defenceRanged) { this._defenceRanged = _defenceRanged}
+    getDefenceRanged() { return this._defenceRanged}
 }
