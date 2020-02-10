36f2e516caf16f9d61ad6d9d4d89126cdb5d6929
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-02-10 15:59:23 +0100

    Can search for a monster now

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ import {MyLocalStorage} from "./modules/MyLocalStorage.js";
 import {Api} from "./modules/Api.js";
 
 class App { 
-    monsters;//todo: kan dit weg?
+    monsters;//MonsterMap class //todo: kan dit weg?
     
     constructor(){
         //vars
@@ -22,16 +22,23 @@ class App {
         //check if there is data in the local storage    
         if(this.myLocalStorage.hasMonsters()){
             //load data from local storage
-            monsters = this.myLocalStorage.get();
+            this.monsters = this.myLocalStorage.get();
         }else{
             //get data from api
             this.api.get();
         }
-        
     }
 
     searchMonster(monsterName){
-        alert(monsterName)
+        if(!this.hasMonsters){
+            this.monsters = this.myLocalStorage.get();
+        }
+
+
+        monsterName = this.capitalizeString(monsterName);
+        const monster = this.monsters.get(monsterName)
+        
+        console.log(monster)
     }
 
     setMonsters(monsters){//monsterMap object
@@ -40,6 +47,11 @@ class App {
 
     hasMonsters(){
         this.monsters.length !== null;
+    }
+
+    //inspired by https://joshtronic.com/2016/02/14/how-to-capitalize-the-first-letter-in-a-string-in-javascript/
+    capitalizeString(string){
+        return string.charAt(0).toUpperCase() + string.substring(1);
     }
 }
 
